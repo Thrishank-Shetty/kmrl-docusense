1f412d6a6d05a4e38010aec1662aa6a62cf3943f
rudxrrrr <15rudraaaaa@gmail.com>
2026-08-19 18:28:55 +0530

    Ignore local database and update config

--- backend/app/config.py
+++ backend/app/config.py
@@ -1,10 +1,13 @@
 import os
+
 from dotenv import load_dotenv
 
 load_dotenv(override=True)
-DATABASE_URL=os.getenv("DATABASE_URL")
-GEMINI_API_KEY=os.getenv("GEMINI_API_KEY")
-GROQ_API_KEY=os.getenv("GROQ_API_KEY")
 
+DATABASE_URL = os.getenv("DATABASE_URL")
+
+GEMINI_API_KEY = os.getenv("GEMINI_API_KEY")
+
+GROQ_API_KEY = os.getenv("GROQ_API_KEY")
 
 
